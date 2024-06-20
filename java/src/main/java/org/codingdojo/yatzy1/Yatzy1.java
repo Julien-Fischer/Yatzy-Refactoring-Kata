483e10a1c4eaa5313139daee758d32b45639fff0
Julien Fischer <julien.fischer@agiledeveloper.net>
2024-06-20 06:25:29 +0200

    refactor: replace the legacy "four_of_a_kind" implementation using the new FourOfAKindStrategy

--- java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
+++ java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
@@ -96,19 +96,9 @@ public class Yatzy1 {
         return new TwoPairStrategy().getScore(roll);
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i+1) * 4;
-        return 0;
+    public static int four_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
+        var roll = new ImmutableRoll(d1, d2, d3, d4, d5);
+        return new FourOfAKindStrategy().getScore(roll);
     }
 
     public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
