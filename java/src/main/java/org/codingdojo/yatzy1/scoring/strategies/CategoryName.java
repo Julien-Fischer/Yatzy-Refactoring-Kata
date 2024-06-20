7e29ef2ba8e1e8ee7c685911a6f42394b5a6fa0b
Julien Fischer <julien.fischer@agiledeveloper.net>
2024-06-20 07:46:52 +0200

    feat: define the CategoryName enum that is one of the center piece of the new design

---
+++ java/src/main/java/org/codingdojo/yatzy1/scoring/strategies/CategoryName.java
@@ -0,0 +1,24 @@
+package org.codingdojo.yatzy1.scoring.strategies;
+
+/**
+ * A {@code CategoryName} uniquely identifies a Yatzy category.
+ */
+public enum CategoryName {
+
+    CHANCE,
+    YATZY,
+    ONES,
+    TWOS,
+    THREES,
+    FOURS,
+    FIVES,
+    SIXES,
+    PAIR,
+    TWO_PAIR,
+    THREE_OF_A_KIND,
+    FOUR_OF_A_KIND,
+    SMALL_STRAIGHT,
+    LARGE_STRAIGHT,
+    FULL_HOUSE;
+
+}
