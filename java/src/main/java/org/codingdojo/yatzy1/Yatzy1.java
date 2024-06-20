e14abb517894b227bac89a8e889063b08c34d496
Julien Fischer <julien.fischer@agiledeveloper.net>
2024-06-20 07:40:21 +0200

    refactor: get rid of the legacy unit tests
    - they are redundant with the new ones that have better coverage
    - they were poorly designed
    - they won't be compatible with the new API we're about to assemble from the new abstractions

--- java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
+++ java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
@@ -74,7 +74,6 @@ public class Yatzy1 {
         return new ThreeOfAKindStrategy().getScore(roll);
     }
 
-
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
         var roll = new ImmutableRoll(d1, d2, d3, d4, d5);
         return new SmallStraightStrategy().getScore(roll);
