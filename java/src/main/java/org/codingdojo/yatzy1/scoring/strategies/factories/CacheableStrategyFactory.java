d8b12a4c968219389b29f8bc5b59d94094e5baab
Julien Fischer <julien.fischer@agiledeveloper.net>
2024-06-20 08:45:59 +0200

    feat: make all CacheableFactory tests pass

--- java/src/main/java/org/codingdojo/yatzy1/scoring/strategies/factories/CacheableStrategyFactory.java
+++ java/src/main/java/org/codingdojo/yatzy1/scoring/strategies/factories/CacheableStrategyFactory.java
@@ -2,8 +2,12 @@ package org.codingdojo.yatzy1.scoring.strategies.factories;
 
 import org.codingdojo.yatzy1.scoring.strategies.CategoryName;
 import org.codingdojo.yatzy1.scoring.strategies.ScoringStrategy;
+import org.codingdojo.yatzy1.scoring.strategies.impl.*;
+import org.codingdojo.yatzy1.scoring.strategies.impl.numbers.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -13,6 +17,7 @@ import java.util.Objects;
 public class CacheableStrategyFactory implements StrategyFactory {
 
     private static CacheableStrategyFactory instance;
+    private final Map<CategoryName, ScoringStrategy> cache = new HashMap<>();
 
     public static CacheableStrategyFactory getInstance() {
         if (instance == null) {
@@ -27,13 +32,35 @@ public class CacheableStrategyFactory implements StrategyFactory {
     @Override
     public ScoringStrategy of(CategoryName name) throws IllegalArgumentException {
         Objects.requireNonNull(name, this::getMessage);
-        // TODO: impl
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (!cache.containsKey(name)) {
+            cache.put(name, createCategory(name));
+        }
+        return cache.get(name);
     }
 
     private String getMessage() {
         return "Category name can not be null.\nSupported values: "
             + Arrays.toString(CategoryName.values());
+    }
+
+    private ScoringStrategy createCategory(CategoryName name) {
+        return switch (name) {
+            case SMALL_STRAIGHT  -> new SmallStraightStrategy();
+            case LARGE_STRAIGHT  -> new LargeStraightStrategy();
+            case ONES            -> new OnesStrategy();
+            case TWOS            -> new TwosStrategy();
+            case THREES          -> new ThreesStrategy();
+            case FOURS           -> new FoursStrategy();
+            case FIVES           -> new FivesStrategy();
+            case SIXES           -> new SixesStrategy();
+            case CHANCE          -> new ChanceStrategy();
+            case PAIR            -> new PairStrategy();
+            case THREE_OF_A_KIND -> new ThreeOfAKindStrategy();
+            case FOUR_OF_A_KIND  -> new FourOfAKindStrategy();
+            case YATZY           -> new YatzyStrategy();
+            case TWO_PAIR        -> new TwoPairStrategy();
+            case FULL_HOUSE      -> new FullHouseStrategy();
+        };
     }
 
 }
