2dd2af24ef9a95031c08886f8ad3d8270752e09e
Julien Fischer <julien.fischer@agiledeveloper.net>
2024-06-20 08:23:10 +0200

    feat: implement getInstance(). Test passes

--- java/src/main/java/org/codingdojo/yatzy1/scoring/strategies/factories/CacheableStrategyFactory.java
+++ java/src/main/java/org/codingdojo/yatzy1/scoring/strategies/factories/CacheableStrategyFactory.java
@@ -9,11 +9,17 @@ import org.codingdojo.yatzy1.scoring.strategies.ScoringStrategy;
  */
 public class CacheableStrategyFactory implements StrategyFactory {
 
+    private static CacheableStrategyFactory instance;
 
     public static CacheableStrategyFactory getInstance() {
-        // TODO: impl
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (instance == null) {
+            instance = new CacheableStrategyFactory();
+        }
+        return instance;
     }
+
+    // Prevent direct instantiation
+    private CacheableStrategyFactory() {}
 
     @Override
     public ScoringStrategy of(CategoryName name) throws IllegalArgumentException {
